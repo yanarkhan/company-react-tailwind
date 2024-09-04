63a8938e5c575335192b46a07e13a4e87c2d7094
yanarkhan <rianarkhan@gmail.com>
2024-09-05 00:20:30 +0700

    setup font dan color

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#010851",
+        secondary: "#9A7AF1",
+        gray: "#707070",
+        pink: "#EE9AE5",
+      },
+    },
+  },
+  plugins: [],
+};
